cf67599279c3e5983acb854e080f30833d53d595
SettleACG <zxxjyn@126.com>
2019-01-13 21:29:19 +0800

    spring boot test

--- spring_test/src/main/java/com/amaduse/controller/UserController.java
+++ spring_test/src/main/java/com/amaduse/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.amaduse.controller;
 
-public class UserController {
+import com.amaduse.entity.User;
+import com.amaduse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("user")
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+    @GetMapping("{name}")
+    public User getUser(@PathVariable("name") String name){
+        return userService.findUserByName(name);
+    }
 }
